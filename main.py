7b3982f16e7527a00b2ec6128238f922c1653f66
Matthew Konyndyk <konyndyk@pdx.edu>
2018-05-29 13:42:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,31 @@ import matplotlib.image as mpimg
 import random
 import numpy as np
 from get_samples import get_samples
+from noisy import noisy
 
 
 #get blocks of training data
-samples = get_samples('lfw-deepfunneled',11000,1) #image folder path, # of samples to collect, 1 for grayscale)
+samples = get_samples('lfw-deepfunneled',11000,0) #image folder path, # of samples to collect,1 for isgrayscale)
 print(samples)
 samples_size = samples.shape
 print(samples_size)
 
 
-#create noisy picture
+#path for full image to test on
+path = '/Users/matthewkonyndyk/Desktop/K-SVD/lfw-deepfunneled/William_Genego/William_Genego_0001.jpg'
+
+#original image
+original_img=mpimg.imread(path)
+imgplot = plt.imshow(original_img)
+plt.show()
+
+#depixelated image
+noisy_img = noisy(path,100,1) #image path, r is the fraction of pixels to change to 0 per block
+#img=mpimg.imread(noisy_img)
+imgplot = plt.imshow(noisy_img)
+plt.show()
+
+
+#reshape images
+noisy_img = np.reshape(noisy_img,187500) # for color images, 250x250x3 = 187500. Each 192 bit section corresponds to a block
+
