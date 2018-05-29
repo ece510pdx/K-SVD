67236bd8926ead2fc12a05ec14d0e4fd928565e1
Matthew Konyndyk <konyndyk@pdx.edu>
2018-05-29 13:43:18 -0700

    Update noisy.py

--- noisy.py
+++ noisy.py
@@ -8,14 +8,15 @@ import numpy as np
 np.set_printoptions(threshold=np.nan)
 
 #adds noise to an image
-def noisy(path,r,ifgrayscale): #folder path for jpg images, # of samples to collect, 1 for grayscale 0 for rbg
-    if grayscale = 0: #if color
+def noisy(path,r,isgrayscale): #folder path for jpg images, # of samples to collect, 1 for grayscale 0 for rbg
+    if isgrayscale == 0: #if color
        #adds noise to color image
        #img = path
         #print(img)
         img=mpimg.imread(path)
         img = img.copy()
-        print(img)
+        original = img.copy()
+        #print(img)
         #imgplot = plt.imshow(img)
         #plt.show()
         
@@ -23,14 +24,13 @@ def noisy(path,r,ifgrayscale): #folder path for jpg images, # of samples to coll
         M = size_Y[0]
         N = size_Y[1]
         Z = size_Y[2]
-        print(M,N,Z)
+        #print(M,N,Z)
         
         counter = r/100*64
         
         while counter > 0:
             for x in range (0,M-8,8):
                 for y in range (0,N-8,8):
-                    
                     a = x + random.randint(0,7)
                     b = y + random.randint(0,7)
                     #print(a,b)
@@ -39,22 +39,20 @@ def noisy(path,r,ifgrayscale): #folder path for jpg images, # of samples to coll
                     img[a,b,2] = 0
             counter -= 1
 
-        imgplot = plt.imshow(img)
-        plt.show()
-        noisy_img = img
-        return noisy_img
 
-if grayscale = 1: #if grayscale
-    #adds noise to color image
-    #img = path
-    #print(img)
-    img=mpimg.imread(path)
-    img = img.copy()
-    print(img)
-    #imgplot = plt.imshow(img)
-    #plt.show()
+
+    if isgrayscale == 1: #if grayscale
+        #adds noise to color image
+        #img = path
+        #print(img)
+        img=mpimg.imread(path)
+        img = img.copy()
+        original_img = img.copy()
+        #print(img)
+        #imgplot = plt.imshow(img)
+        #plt.show()
     
-    size_Y = img.shape
+        size_Y = img.shape
         M = size_Y[0]
         N = size_Y[1]
         
@@ -71,7 +69,8 @@ if grayscale = 1: #if grayscale
                     img[a,b,1] = 0
             counter -= 1
 
-    imgplot = plt.imshow(img)
-        plt.show()
-        noisy_img = img
-        return noisy_img
+    #imgplot = plt.imshow(img)
+    #plt.show()
+
+    noisy_img = img
+    return noisy_img
